bitwarden/remote-access

Simplify Docker build: cross-compile + runtime-only Caddy image
- Replace multi-stage builder with runtime-only FROM caddy:2.9.1-alpine
- Cross-compile to x86_64-unknown-linux-musl with `cross` locally; send
  a 4-file context (binary + Caddyfile + entrypoint + Dockerfile) to both
  local docker build and az acr build — no AWK, no Cargo.toml rewriting
- Switch tokio-tungstenite from native-tls to rustls-tls-native-roots to
  eliminate the OpenSSL build dependency
- Bake VERSION=<git-sha>-<timestamp> build arg into the image; Caddy
  serves it via {env.VERSION} on the root endpoint
- Add .dockerignore to exclude target/ and .git/ from any full-repo builds

scripts/docker-build.sh
@@ -5,6 +5,7 @@ IMAGE_NAME="bw-proxy"
 TAG="latest"
 REGISTRY=""
 ACR=false
+TARGET="x86_64-unknown-linux-musl"
 
 usage() {
     echo "Usage: $0 [OPTIONS]"
@@ -15,12 +16,13 @@ usage() {
     echo "  --name NAME        Image name (default: bw-proxy)"
     echo "  --tag TAG          Image tag (default: latest)"
     echo "  --registry NAME    ACR registry name (e.g. myregistry) for --acr mode"
-    echo "  --acr              Build in Azure Container Registry (linux/amd64)"
+    echo "  --acr              Build and push to Azure Container Registry"
+    echo "  --target TARGET    Cross-compilation target (default: x86_64-unknown-linux-musl)"
     echo "  -h, --help         Show this help message"
     echo ""
     echo "Examples:"
-    echo "  $0                                     # Build locally (native arch)"
-    echo "  $0 --acr --registry myregistry         # Build linux/amd64 in ACR"
+    echo "  $0                                     # Cross-compile + build locally"
+    echo "  $0 --acr --registry myregistry         # Cross-compile + build in ACR"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -29,6 +31,7 @@ while [[ $# -gt 0 ]]; do
         --tag) TAG="$2"; shift 2 ;;
         --registry) REGISTRY="$2"; shift 2 ;;
         --acr) ACR=true; shift ;;
+        --target) TARGET="$2"; shift 2 ;;
         -h|--help) usage; exit 0 ;;
         *) echo "Unknown option: $1"; usage; exit 1 ;;
     esac
@@ -38,68 +41,47 @@ REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 PROXY_DIR="$REPO_ROOT/crates/bw-proxy"
 FULL_IMAGE="${IMAGE_NAME}:${TAG}"
 
-# Assemble a minimal build context in a temp directory.
-# The Dockerfile expects a workspace layout (Cargo.toml, Cargo.lock, crates/),
-# but we only include the crates bw-proxy actually depends on and trim the
-# workspace members list so cargo doesn't look for the others.
-assemble_context() {
-    local ctx="$1"
-    cp "$PROXY_DIR/Dockerfile" "$ctx/"
-    cp "$REPO_ROOT/Cargo.lock" "$ctx/"
+GIT_SHA="$(git -C "$REPO_ROOT" rev-parse --short HEAD 2>/dev/null || echo "unknown")"
+BUILD_TIME="$(date -u +%Y%m%d-%H%M%S)"
+VERSION="${GIT_SHA}-${BUILD_TIME}"
 
-    # Copy workspace Cargo.toml with only the required members
-    awk '
-        /^members/ { in_members=1 }
-        in_members && /\]/ {
-            print "members = ["
-            print "    \"crates/bw-error\","
-            print "    \"crates/bw-error-macro\","
-            print "    \"crates/bw-proxy\","
-            print "]"
-            in_members=0
-            next
-        }
-        in_members == 0 { print }
-    ' "$REPO_ROOT/Cargo.toml" > "$ctx/Cargo.toml"
+# Phase 1: Cross-compile
+echo "Cross-compiling bw-proxy for ${TARGET}..."
+cross build --release -p bw-proxy --target "$TARGET"
 
-    # Copy only the crates bw-proxy depends on
-    mkdir -p "$ctx/crates"
-    for crate in bw-proxy bw-error bw-error-macro; do
-        cp -r "$REPO_ROOT/crates/$crate" "$ctx/crates/"
-    done
+BINARY="$REPO_ROOT/target/${TARGET}/release/bw-proxy"
+if [[ ! -f "$BINARY" ]]; then
+    echo "Error: binary not found at $BINARY"
+    exit 1
+fi
 
-    # Caddy reverse proxy config and entrypoint
-    cp "$PROXY_DIR/Caddyfile" "$ctx/crates/bw-proxy/"
-    cp "$PROXY_DIR/entrypoint.sh" "$ctx/crates/bw-proxy/"
-
-    # Optional Zscaler CA cert (empty file if not present)
-    if [[ -f "$PROXY_DIR/extra-root-ca.crt" ]]; then
-        cp "$PROXY_DIR/extra-root-ca.crt" "$ctx/"
-    else
-        touch "$ctx/extra-root-ca.crt"
-    fi
-
-    echo "Build context: $(du -sh "$ctx" | cut -f1)"
-}
-
+# Phase 2: Assemble minimal Docker context (4 flat files)
 CTX="$(mktemp -d)"
 trap "rm -rf '$CTX'" EXIT
-assemble_context "$CTX"
 
+cp "$BINARY"                        "$CTX/bw-proxy"
+cp "$PROXY_DIR/Dockerfile"          "$CTX/Dockerfile"
+cp "$PROXY_DIR/Caddyfile"           "$CTX/Caddyfile"
+cp "$PROXY_DIR/entrypoint.sh"       "$CTX/entrypoint.sh"
+
+echo "Build context: $(du -sh "$CTX" | cut -f1)"
+
+# Phase 3: Build image
 if $ACR; then
     if [[ -z "$REGISTRY" ]]; then
         echo "Error: --registry is required with --acr"
         exit 1
     fi
 
-    echo "Building ${FULL_IMAGE} in ACR (${REGISTRY}.azurecr.io)..."
+    echo "Building ${FULL_IMAGE} in ACR (${REGISTRY}.azurecr.io)... [${VERSION}]"
     az acr build \
         --registry "$REGISTRY" \
         --image "$FULL_IMAGE" \
         --platform linux/amd64 \
+        --build-arg "VERSION=${VERSION}" \
         "$CTX"
 else
-    echo "Building ${FULL_IMAGE} locally..."
-    docker build -t "$FULL_IMAGE" "$CTX"
+    echo "Building ${FULL_IMAGE} locally... [${VERSION}]"
+    docker build -t "$FULL_IMAGE" --build-arg "VERSION=${VERSION}" "$CTX"
     echo "Loaded ${FULL_IMAGE} into local Docker"
 fi

bitwarden/agent-access

scripts/docker-build.sh
@@ -5,6 +5,7 @@ IMAGE_NAME="ap-proxy"
 TAG="latest"
 REGISTRY=""
 ACR=false
+TARGET="x86_64-unknown-linux-musl"
 
 usage() {
     echo "Usage: $0 [OPTIONS]"
@@ -15,12 +16,13 @@ usage() {
     echo "  --name NAME        Image name (default: ap-proxy)"
     echo "  --tag TAG          Image tag (default: latest)"
     echo "  --registry NAME    ACR registry name (e.g. myregistry) for --acr mode"
-    echo "  --acr              Build in Azure Container Registry (linux/amd64)"
+    echo "  --acr              Build and push to Azure Container Registry"
+    echo "  --target TARGET    Cross-compilation target (default: x86_64-unknown-linux-musl)"
     echo "  -h, --help         Show this help message"
     echo ""
     echo "Examples:"
-    echo "  $0                                     # Build locally (native arch)"
-    echo "  $0 --acr --registry myregistry         # Build linux/amd64 in ACR"
+    echo "  $0                                     # Cross-compile + build locally"
+    echo "  $0 --acr --registry myregistry         # Cross-compile + build in ACR"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -29,6 +31,7 @@ while [[ $# -gt 0 ]]; do
         --tag) TAG="$2"; shift 2 ;;
         --registry) REGISTRY="$2"; shift 2 ;;
         --acr) ACR=true; shift ;;
+        --target) TARGET="$2"; shift 2 ;;
         -h|--help) usage; exit 0 ;;
         *) echo "Unknown option: $1"; usage; exit 1 ;;
     esac
@@ -38,66 +41,47 @@ REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 PROXY_DIR="$REPO_ROOT/crates/ap-proxy"
 FULL_IMAGE="${IMAGE_NAME}:${TAG}"
 
-# Assemble a minimal build context in a temp directory.
-# The Dockerfile expects a workspace layout (Cargo.toml, Cargo.lock, crates/),
-# but we only include the crates ap-proxy actually depends on and trim the
-# workspace members list so cargo doesn't look for the others.
-assemble_context() {
-    local ctx="$1"
-    cp "$PROXY_DIR/Dockerfile" "$ctx/"
-    cp "$REPO_ROOT/Cargo.lock" "$ctx/"
+GIT_SHA="$(git -C "$REPO_ROOT" rev-parse --short HEAD 2>/dev/null || echo "unknown")"
+BUILD_TIME="$(date -u +%Y%m%d-%H%M%S)"
+VERSION="${GIT_SHA}-${BUILD_TIME}"
 
-    # Copy workspace Cargo.toml with only the required members
-    awk '
-        /^members/ { in_members=1 }
-        in_members && /\]/ {
-            print "members = ["
-            print "    \"crates/ap-error\","
-            print "    \"crates/ap-error-macro\","
-            print "    \"crates/ap-proxy\","
-            print "    \"crates/ap-proxy-client\","
-            print "    \"crates/ap-proxy-protocol\","
-            print "]"
-            in_members=0
-            next
-        }
-        in_members == 0 { print }
-    ' "$REPO_ROOT/Cargo.toml" > "$ctx/Cargo.toml"
+# Phase 1: Cross-compile
+echo "Cross-compiling ap-proxy for ${TARGET}..."
+cross build --release -p ap-proxy --target "$TARGET"
 
-    # Copy only the crates ap-proxy depends on
-    mkdir -p "$ctx/crates"
-    for crate in ap-proxy ap-proxy-client ap-proxy-protocol ap-error ap-error-macro; do
-        cp -r "$REPO_ROOT/crates/$crate" "$ctx/crates/"
-    done
+BINARY="$REPO_ROOT/target/${TARGET}/release/ap-proxy"
+if [[ ! -f "$BINARY" ]]; then
+    echo "Error: binary not found at $BINARY"
+    exit 1
+fi
 
-    # Optional Zscaler CA cert (empty file if not present)
-    if [[ -f "$PROXY_DIR/extra-root-ca.crt" ]]; then
-        cp "$PROXY_DIR/extra-root-ca.crt" "$ctx/"
-    else
-        touch "$ctx/extra-root-ca.crt"
-    fi
-
-    echo "Build context: $(du -sh "$ctx" | cut -f1)"
-}
-
+# Phase 2: Assemble minimal Docker context (4 flat files)
 CTX="$(mktemp -d)"
 trap "rm -rf '$CTX'" EXIT
-assemble_context "$CTX"
 
+cp "$BINARY"                        "$CTX/ap-proxy"
+cp "$PROXY_DIR/Dockerfile"          "$CTX/Dockerfile"
+cp "$PROXY_DIR/Caddyfile"           "$CTX/Caddyfile"
+cp "$PROXY_DIR/entrypoint.sh"       "$CTX/entrypoint.sh"
+
+echo "Build context: $(du -sh "$CTX" | cut -f1)"
+
+# Phase 3: Build image
 if $ACR; then
     if [[ -z "$REGISTRY" ]]; then
         echo "Error: --registry is required with --acr"
         exit 1
     fi
 
-    echo "Building ${FULL_IMAGE} in ACR (${REGISTRY}.azurecr.io)..."
+    echo "Building ${FULL_IMAGE} in ACR (${REGISTRY}.azurecr.io)... [${VERSION}]"
     az acr build \
         --registry "$REGISTRY" \
         --image "$FULL_IMAGE" \
         --platform linux/amd64 \
+        --build-arg "VERSION=${VERSION}" \
         "$CTX"
 else
-    echo "Building ${FULL_IMAGE} locally..."
-    docker build -t "$FULL_IMAGE" "$CTX"
+    echo "Building ${FULL_IMAGE} locally... [${VERSION}]"
+    docker build -t "$FULL_IMAGE" --build-arg "VERSION=${VERSION}" "$CTX"
     echo "Loaded ${FULL_IMAGE} into local Docker"
 fi